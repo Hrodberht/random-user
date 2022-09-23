6c2e7b40ddd3736711f96fe737c88aa3058cd33e
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-23 09:23:05 +0100

    Selected DOM elements using getElement.js

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ import get from "./utils/getElement.js";
 const URL = "https://randomuser.me/api/";
 
 const img = get(".user-img");
+const title = get(".user-title");
+const value = get(".user-value");
+const btn = get(".btn");
+const btns = [...document.querySelectorAll(".icon")];
