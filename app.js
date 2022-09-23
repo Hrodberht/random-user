c587e8f1d1bfe08eeb5bc2725e2418cb62e083a8
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-23 09:39:39 +0100

    Added fetch user functionality in module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import get from "./utils/getElement.js";
+import getUser from "./utils/fetchUser.js";
 
 const URL = "https://randomuser.me/api/";
 
@@ -7,3 +8,11 @@ const title = get(".user-title");
 const value = get(".user-value");
 const btn = get(".btn");
 const btns = [...document.querySelectorAll(".icon")];
+
+const showUser = async () => {
+  const person = await getUser();
+  console.log(person);
+};
+
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
