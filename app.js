fb0734cb459391e0e7bb999beebce22836b0f85d
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-23 09:16:54 +0100

    Added getElement function

---
+++ app.js
@@ -0,0 +1,9 @@
+const getElement = (selection) => {
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error("no element selected");
+};
+
+const URL = "https://randomuser.me/api/";
+
+const img = getElement(".user-img");
